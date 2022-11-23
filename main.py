340bd6da28264418ead9f9e3aea663aed4ac0e1a
babucarr32 <baboucarry@gmail.com>
2022-11-23 15:27:37 +0000

    Added the search url functionality

--- main.py
+++ main.py
@@ -15,7 +15,17 @@ def terminaleBrowser():
         r = requests.get(str(sys.argv[2]))
         soup = BeautifulSoup(r.text, "lxml")
         print(soup.get_text())
-    # for j in search(query, tld="co.in", num=10, stop=10, pause=2):
-        # print(j)
+
+    if "s" in sys.argv:
+        print("------------------------------------------------------------")
+        print(f"RESULTS FOR SEARCH: {sys.argv[2]}")
+        print("-----------------------------------------------------------")
+        for j in search(query, tld="co.in", num=10, stop=10, pause=2):
+            print(j)
+    else:
+        r = requests.get(str(sys.argv[2]))
+        soup = BeautifulSoup(r.text, "lxml")
+        print(soup.prettify())
+
 
 terminaleBrowser()
