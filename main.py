625e4a8fc53eb47e83e36c6b4f2bb19b442aa83e
babucarr32 <baboucarry@gmail.com>
2022-11-23 15:51:36 +0000

    Added the search with few details about the search

--- main.py
+++ main.py
@@ -8,24 +8,40 @@ except ImportError:
 	print("No module named 'google' found")
 
 def terminaleBrowser():
-    # to search
-    query = "A computer science portal"
+    try:
+        # to search
+        query = str(sys.argv[2])
+        if "o" in sys.argv:
+            r = requests.get(str(sys.argv[2]))
+            soup = BeautifulSoup(r.text, "lxml")
+            print(soup.get_text())
 
-    if "o" in sys.argv:
-        r = requests.get(str(sys.argv[2]))
-        soup = BeautifulSoup(r.text, "lxml")
-        print(soup.get_text())
-
-    if "s" in sys.argv:
-        print("------------------------------------------------------------")
-        print(f"RESULTS FOR SEARCH: {sys.argv[2]}")
-        print("-----------------------------------------------------------")
-        for j in search(query, tld="co.in", num=10, stop=10, pause=2):
-            print(j)
-    else:
-        r = requests.get(str(sys.argv[2]))
-        soup = BeautifulSoup(r.text, "lxml")
-        print(soup.prettify())
-
+        elif "s" in sys.argv:
+            print("------------------------------------------------------------")
+            print(f"SEARCH RESULTS FOR: {sys.argv[2]}")
+            print("-----------------------------------------------------------")
+            for j in search(query, tld="co.in", num=10, stop=20, pause=2):
+                print(j)
+        elif "sV" in sys.argv:
+            print("------------------------------------------------------------")
+            print(f"SEARCH RESULTS FOR: {sys.argv[2]}")
+            print("-----------------------------------------------------------")
+           
+            for j in search(query, tld="co.in", num=10, stop=20, pause=3):
+                try:
+                    print(f"WEBSITE: {j}\n")
+                    r = requests.get(str(j))
+                    soup = BeautifulSoup(r.text, "lxml")
+                    print(soup.get_text().replace("\n", '')[:300])
+                    print("\n")
+                    print("-----------------------------------------------------------------\n")
+                except Exception as e:
+                    continue
+        else:
+            r = requests.get(str(sys.argv[2]))
+            soup = BeautifulSoup(r.text, "lxml")
+            print(soup.prettify())
+    except Exception as e:
+        pass
 
 terminaleBrowser()
