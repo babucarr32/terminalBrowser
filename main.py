cc1503d58a51e39d084542aa8fc965770bc5363d
babucarr32 <baboucarry@gmail.com>
2022-11-28 20:38:05 +0000

    Added the banner

--- main.py
+++ main.py
@@ -7,44 +7,67 @@ try:
 except ImportError:
 	print("No module named 'google' found")
 
-def terminaleBrowser():
-    if sys.argv == 0:
-        print(f"Usage: <script>, <arg>, <search>")
-        quit()
-    try:
-        # to search
-        query = str(sys.argv[2])
-        if "o" in sys.argv:
-            r = requests.get(str(sys.argv[2]))
-            soup = BeautifulSoup(r.text, "lxml")
-            print(soup.get_text())
+def helper():
+    print("-----------------------------------")
+    print("Example: main.py o www.target.com")
+    print("Example: main.py sV python")
+    print("Example: main.py python")
+    print("----------------------------------")
+    print("""
+    Comands:
+            raw: Display the text of a site(s).
+            hint: Display some info about the site(s).
+            prettify: Display the text of a site(s), with prettify.
 
-        elif "s" in sys.argv:
-            print("------------------------------------------------------------")
-            print(f"SEARCH RESULTS FOR: {sys.argv[2]}")
-            print("-----------------------------------------------------------")
-            for j in search(query, tld="co.in", num=10, stop=20, pause=2):
-                print(j)
-        elif "sV" in sys.argv:
-            print("------------------------------------------------------------")
-            print(f"SEARCH RESULTS FOR: {sys.argv[2]}")
-            print("-----------------------------------------------------------")
-           
-            for j in search(query, tld="co.in", num=10, stop=20, pause=3):
-                try:
-                    print(f"WEBSITE: {j}\n")
-                    r = requests.get(str(j))
-                    soup = BeautifulSoup(r.text, "lxml")
-                    print(soup.get_text().replace("\n", '')[:300] + "...")
-                    print("\n")
-                    print("-----------------------------------------------------------------\n")
-                except Exception as e:
-                    continue
-        else:
-            r = requests.get(str(sys.argv[2]))
-            soup = BeautifulSoup(r.text, "lxml")
-            print(soup.prettify())
-    except Exception as e:
+    """)
+
+
+def terminaleBrowser():
+    if len(sys.argv) == 1:
+        print(f"Command Usage: [<script>, <arg>, <search>]")
+        print("Type --help: to show the usage...")
+        quit()
+    # to search
+    
+    if "raw" in sys.argv:
+        r = requests.get(str(sys.argv[2]))
+        soup = BeautifulSoup(r.text, "html.parser")
+        print(soup.get_text())
+
+    elif "prettify" in sys.argv:
+        r = requests.get(str(sys.argv[2]))
+        soup = BeautifulSoup(r.text, "html.parser")
+        with open("index.html", "w") as f:
+            f.write(soup.prettify())
+        # print(soup.prettify())
+    elif "hint" in sys.argv:
         pass
+        print("------------------------------------------------------------")
+        print(f"SEARCH RESULTS FOR: {sys.argv[2]}")
+        print("-----------------------------------------------------------")
+
+        query = str(sys.argv[2])
+        for j in search(query, tld="co.in", num=10, stop=20, pause=3):
+            try:
+                print(f"WEBSITE: {j}\n")
+                r = requests.get(str(j))
+                soup = BeautifulSoup(r.text, "html.parser")
+                print(soup.get_text().replace("\n", '')[:300] + "...")
+                print("\n")
+                print("-----------------------------------------------------------------\n")
+            except Exception as e:
+                continue
+    elif "--help" in sys.argv:
+        helper()
+    else:
+        print(True)
+        print(True)
+        query = str(sys.argv[1])
+        print(query)
+        print("------------------------------------------------------------")
+        print(f"SEARCH RESULTS FOR: {sys.argv[1]}")
+        print("-----------------------------------------------------------")
+        for j in search(query, tld="co.in", num=10, stop=20, pause=2):
+            print(j)
 
 terminaleBrowser()
